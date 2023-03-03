bc5e185a9e7489409b07ac77569ff0ce363552d7
Leo Heck <leandroheck@gmail.com>
2023-03-03 14:17:48 -0500

    first try

---
+++ routes/morse.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const morseController = require('../controllers/morse') 
+const { ensureAuth } = require('../middleware/auth')
+
+router.get('/', ensureAuth, morseController.getMorse)
+
+router.get('/suggest', ensureAuth, morseController.getSuggest)
+
+router.get('/scores', ensureAuth, morseController.getScores)
+
+router.put('/updatescore', morseController.updateScore)
+
+router.post('/createMorse', morseController.createMorse)
+
+module.exports = router
