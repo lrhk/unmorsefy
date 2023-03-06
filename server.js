938c7743409023a87c5d5cc50abbe8f70d040fd5
Leo Heck <leandroheck@gmail.com>
2023-03-06 11:56:33 -0500

    session modified

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
-const PORT = process.env.PORT || 5000
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const morseRoutes = require('./routes/morse')
@@ -30,7 +29,7 @@ app.use(
       secret: 'keyboard cat',
       resave: false,
       saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
     })
   )
   
@@ -43,6 +42,8 @@ app.use(flash())
 app.use('/', mainRoutes)
 app.use('/morse', morseRoutes)
  
+const PORT = process.env.PORT || 5000
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
